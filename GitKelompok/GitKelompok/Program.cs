fee3d719dbb335c2c6d29f7278d279ed3ee4a5b7
Mario Sebastian Carlosiano <carlosiano@students.amikom.ac.id>
2021-12-07 05:29:02 +0700

    menambahkan operasi penjumblahan (Mario Sebatian Carlosiano_20.11.3439)

--- GitKelompok/GitKelompok/Program.cs
+++ GitKelompok/GitKelompok/Program.cs
@@ -16,6 +16,11 @@ namespace GitKelompok
             int angka1 = 20, angka2 = 10;
             int perjumlahan = 0, pengurangan = 0, perkalian = 0;
             double pembagian = 0;
+
+            // menambahkan operasi penjumblahan
+            perjumlahan = angka1 + angka2;
+            Console.WriteLine("{0} + {1} = {2}", angka1, angka2, perjumlahan);
+
         }
     }
 }
