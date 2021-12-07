1a0ddc4563fd4bc4a90c7c9a3cf192fd79252951
Dewa <syabdewa02@gmail.com>
2021-12-07 08:02:45 +0700

    Membuat algoritma aritmatika (Muhammad Syabdewa 20.11.3438)

--- GitKelompok/GitKelompok/Program.cs
+++ GitKelompok/GitKelompok/Program.cs
@@ -11,6 +11,11 @@ namespace GitKelompok
         static void Main(string[] args)
         {
             Console.WriteLine("Latihan GIT Kelompok");
+            Console.WriteLine("Program Aritmatika");
+
+            int angka1 = 20, angka2 = 10;
+            int perjumlahan = 0, pengurangan = 0, perkalian = 0;
+            double pembagian = 0;
         }
     }
 }
