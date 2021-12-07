a4538b3a173bf49ea15d1911d7113d9a19633003
Dewa <syabdewa02@gmail.com>
2021-12-07 07:52:53 +0700

    Menmbah code console.writeline

---
+++ GitKelompok/GitKelompok/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitKelompok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Latihan GIT Kelompok");
+        }
+    }
+}
